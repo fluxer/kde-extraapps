22a77e79017dd5d9eae83e2d6798b6add4db5a82
Ivailo Monev <xakepa10@gmail.com>
2015-01-25 01:54:53 +0000

    kuassel: remove redudant shortcuts and key sequences source files

--- kuassel/src/qtui/settingspages/settingspages.cmake
+++ kuassel/src/qtui/settingspages/settingspages.cmake
@@ -43,8 +43,3 @@ set(SP_FORMS
     saveidentitiesdlg.ui
     servereditdlg.ui
 )
-
-if (NOT KDE4_FOUND)
-    list(APPEND SETTINGSPAGES shortcuts)
-    list(APPEND SP_SOURCES keysequencewidget.cpp shortcutsmodel.cpp)
-endif()
