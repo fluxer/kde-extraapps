7c95601a4e67122183e504f28f4728cd2dee7ba7
Ivailo Monev <xakepa10@gmail.com>
2015-09-21 22:31:59 +0000

    kvkbd: fix crash due to wrong cli arguments implementation

--- kvkbd/src/main.cpp
+++ kvkbd/src/main.cpp
@@ -83,16 +83,16 @@ int main(int argc, char **argv)
     options.add("loginhelper", ki18n("Stand alone version for use with KDM or XDM.\n"
                                      "See Kvkbd Handbook for information on how to use this option."));
     KCmdLineArgs::addCmdLineOptions(options);
+    KUniqueApplication::addCmdLineOptions();
 
-    bool is_login = KCmdLineArgs::parsedArgs()->isSet("loginhelper");
+    const bool is_login = KCmdLineArgs::parsedArgs()->isSet("loginhelper");
     if (!is_login) {
       findLoginWindow();
     }
-	
+
     KvkbdApp app(is_login);
 
     return app.exec();
-    
 }
 
 
