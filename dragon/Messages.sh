5211ced3ae3e1838e3b04d015ecccd6d987575d6
Ivailo Monev <xakepa10@gmail.com>
2016-03-17 01:23:48 +0000

    dragon: drop in favour of kmediaplayer from kde-baseapps
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- dragon/Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/dragonplayer.pot
-rm -f rc.cpp
