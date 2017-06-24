e6100511085f6afed33a60693e628a5e6cac1897
Ivailo Monev <xakepa10@laimg.moc>
2017-06-24 07:13:30 +0000

    generic: misc cleanups
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- kmix/ConfigureChecks.cmake
+++ kmix/ConfigureChecks.cmake
@@ -3,19 +3,7 @@ include(CheckTypeSize)
 include(CheckStructMember)
 include(MacroBoolTo01)
 
-# The FindKDE4.cmake module sets _KDE4_PLATFORM_DEFINITIONS with
-# definitions like _GNU_SOURCE that are needed on each platform.
-set(CMAKE_REQUIRED_DEFINITIONS ${_KDE4_PLATFORM_DEFINITIONS})
-
 macro_bool_to_01(OGGVORBIS_FOUND HAVE_VORBIS)
-
-#now check for dlfcn.h using the cmake supplied CHECK_include_FILE() macro
-# If definitions like -D_GNU_SOURCE are needed for these checks they
-# should be added to _KDE4_PLATFORM_DEFINITIONS when it is originally
-# defined outside this file.  Here we include these definitions in
-# CMAKE_REQUIRED_DEFINITIONS so they will be included in the build of
-# checks below.
-set(CMAKE_REQUIRED_DEFINITIONS ${_KDE4_PLATFORM_DEFINITIONS})
 
 check_include_files(machine/endian.h HAVE_MACHINE_ENDIAN_H)
 # Linux has <endian.h>, FreeBSD has <sys/endian.h> and Solaris has neither.
