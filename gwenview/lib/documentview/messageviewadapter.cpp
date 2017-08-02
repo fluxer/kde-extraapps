d9d390e7f75f94dd51f471792a0669b2a6691a7c
Ivailo Monev <xakepa10@laimg.moc>
2017-08-02 14:59:14 +0000

    gwenview: adjust to recent CMake changes
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- gwenview/lib/documentview/messageviewadapter.cpp
+++ gwenview/lib/documentview/messageviewadapter.cpp
@@ -30,7 +30,7 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA
 
 // Local
 #include <lib/document/document.h>
-#include <lib/ui_messageview.h>
+#include <ui_messageview.h>
 
 namespace Gwenview
 {
