e20c8e46e96984a4775db0e4fc78088a07ce766c
Ivailo Monev <xakepa10@laimg.moc>
2020-02-10 01:22:40 +0000

    krfb: drop SLP support
    TODO from https://github.com/fluxer/katana/issues/8
    
    Signed-off-by: Ivailo Monev <xakepa10@laimg.moc>

--- krfb/krfb/config-krfb.h.cmake
+++ krfb/krfb/config-krfb.h.cmake
@@ -1,9 +1,6 @@
 /* Define to 1 if you have the `getifaddrs' function. */
 #cmakedefine HAVE_GETIFADDRS 1
 
-/* Define if SLP is available */
-#cmakedefine HAVE_SLP 1
-
 /* Define if XDamage is available */
 #cmakedefine HAVE_XDAMAGE 1
 
