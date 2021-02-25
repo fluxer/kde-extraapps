3cd4937d2a8ac71b865874e06171af2a58029f4c
Ivailo Monev <xakepa10@gmail.com>
2021-02-25 22:49:44 +0200

    okular: restore behaviour prior to 89a76dc6cadda129b2054ead035ae834937d7ed4
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- okular/core/audioplayer.cpp
+++ okular/core/audioplayer.cpp
@@ -102,7 +102,7 @@ bool AudioPlayerPrivate::play( const SoundInfo& si )
     kDebug() ;
     PlayData * data = new PlayData();
     data->m_player = new KAudioPlayer();
-    // data->m_player->setVolume( si.volume );
+    data->m_player->setVolume( float(si.volume) );
     data->m_info = si;
     bool valid = false;
 
