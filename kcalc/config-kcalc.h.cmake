fd395fe48a49b287c2f48315fdf881ca0ee4fae8
Ivailo Monev <xakepa10@gmail.com>
2021-09-14 22:45:51 +0300

    kcalc: remove unused configuration checks
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- kcalc/config-kcalc.h.cmake
+++ kcalc/config-kcalc.h.cmake
@@ -1,21 +1,3 @@
-/* Define to 1 if you have the <ieeefp.h> header file. */
-#cmakedefine HAVE_IEEEFP_H 1
-
-/* Define if you have isinf */
-#cmakedefine HAVE_FUNC_ISINF 1
-
-/* Define if you have isnan */
-#cmakedefine HAVE_FUNC_ISNAN 1
-
-/* Define if you have round */
-#define HAVE_FUNC_ROUNDL 1
-
-/* Define if you have round */
-#define HAVE_FUNC_ROUND 1
-
-/* Define if you have libgmp */
-#define HAVE_GMP 1
-
 /* Define if you have support for long double in printf */
 #define HAVE_LONG_DOUBLE 1
 
