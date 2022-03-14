a932a2e3c1a2b150e57f1ca09df7eebc5777a357
Ivailo Monev <xakepa10@gmail.com>
2022-03-14 08:03:06 +0200

    print-manager: removing it, use the web browser interface instead
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- print-manager/Messages.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC `find . -name "*.rc" -o -name "*.ui" | grep -v "plasmoid"` >> rc.cpp
-$XGETTEXT `find . -name "*.cpp" | grep -v "plasmoid"` -o $podir/print-manager.pot
-rm -f rc.cpp
-
-$EXTRACTRC `find plasmoid -name "*.rc" -o -name "*.ui"` >> rc.cpp
-$XGETTEXT rc.cpp `find plasmoid -name "*.cpp"` -o $podir/plasma_applet_org.kde.printmanager.pot
-$XGETTEXT `find plasmoid -name "*.qml"` -j -L Java -o $podir/plasma_applet_org.kde.printmanager.pot
