2903a9b6b657ef008e332d32c85d4ffd0ce6d467
Ivailo Monev <xakepa10@gmail.com>
2022-05-19 19:40:24 +0300

    gwenview: remove now unused QtConcurrentRun header inclusion
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- gwenview/lib/document/loadingdocumentimpl.cpp
+++ gwenview/lib/document/loadingdocumentimpl.cpp
@@ -31,7 +31,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 #include <QImage>
 #include <QImageReader>
 #include <QPointer>
-#include <QtConcurrentRun>
 
 // KDE
 #include <KDebug>
