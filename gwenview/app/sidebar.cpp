48d2993857b23a2107e80551a260a806c28e2347
Ivailo Monev <xakepa10@gmail.com>
2022-09-28 17:34:57 +0300

    gwenview: remove unused header inclusion in sidebar source file
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- gwenview/app/sidebar.cpp
+++ gwenview/app/sidebar.cpp
@@ -37,7 +37,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include <KIcon>
 #include <KIconLoader>
 #include <KGlobalSettings>
-#include <KWordWrap>
 
 // Local
 
