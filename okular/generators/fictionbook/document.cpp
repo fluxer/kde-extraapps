02694b56cf46baa77c4cee5cece9dd3a6cf5077b
Ivailo Monev <xakepa10@gmail.com>
2022-10-13 19:25:29 +0300

    okular: deal with FIXME in fictionbook generator
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- okular/generators/fictionbook/document.cpp
+++ okular/generators/fictionbook/document.cpp
@@ -10,7 +10,6 @@
 #include "document.h"
 
 #include <QtCore/QFile>
-#include <QtCore/QBuffer>
 
 #include <klocale.h>
 #include <karchive.h>
@@ -24,49 +23,47 @@ Document::Document( const QString &fileName )
 
 bool Document::open()
 {
-    QIODevice *device;
-
-    QFile file( mFileName );
     if ( mFileName.endsWith( ".fb" ) || mFileName.endsWith( ".fb2" ) ) {
+        QFile file( mFileName );
         if ( !file.open( QIODevice::ReadOnly ) ) {
             setError( i18n( "Unable to open document: %1", file.errorString() ) );
             return false;
         }
 
-        device = &file;
-    } else {
-        KArchive zip( mFileName );
-        if ( !zip.isReadable() ) {
-            setError( i18n( "Document is not a valid ZIP archive" ) );
+        QString errorMsg;
+        if ( !mDocument.setContent( &file, true, &errorMsg ) ) {
+            setError( i18n( "Invalid XML document: %1", errorMsg ) );
             return false;
         }
-
-        const QList<KArchiveEntry> entries = zip.list();
-
-        QString documentFile;
-        for ( int i = 0; i < entries.count(); ++i ) {
-            if ( entries[ i ].pathname.endsWith( ".fb2" ) ) {
-                documentFile = QFile::decodeName(entries[ i ].pathname);
-                break;
-            }
-        }
-
-        if ( documentFile.isEmpty() ) {
-            setError( i18n( "No content found in the document" ) );
-            return false;
-        }
-
-        // FIXME delete 'deviceì somewhen
-        device = new QBuffer();
-        qobject_cast<QBuffer*>(device)->setData( zip.data( documentFile ) );
+        return true;
     }
 
-    QString errorMsg;
-    if ( !mDocument.setContent( device, true, &errorMsg ) ) {
-        setError( i18n( "Invalid XML document: %1", errorMsg ) );
+    KArchive zip( mFileName );
+    if ( !zip.isReadable() ) {
+        setError( i18n( "Document is not a valid ZIP archive" ) );
         return false;
     }
 
+    const QList<KArchiveEntry> entries = zip.list();
+
+    QString documentFile;
+    for ( int i = 0; i < entries.count(); ++i ) {
+        if ( entries[ i ].pathname.endsWith( ".fb2" ) ) {
+            documentFile = QFile::decodeName(entries[ i ].pathname);
+            break;
+        }
+    }
+
+    if ( documentFile.isEmpty() ) {
+        setError( i18n( "No content found in the document" ) );
+        return false;
+    }
+
+    QString errorMsg;
+    if ( !mDocument.setContent( zip.data( documentFile ), true, &errorMsg ) ) {
+        setError( i18n( "Invalid XML document: %1", errorMsg ) );
+        return false;
+    }
     return true;
 }
 
