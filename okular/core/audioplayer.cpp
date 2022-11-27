8ed83be2bf80799f2dcb4d3c698bb12dbd972751
Ivailo Monev <xakepa10@gmail.com>
2022-11-27 21:35:10 +0200

    okular: map the sound action volume to value appropriate for KAudioPlayer
    fixes regression since the port to KAudioPlayer, the use of qBound() is
    there to make sure speakers (or head-phones) do not blow up while the
    assert is for developers to know that Okular generator is trying to use
    value outside the range
    
    Signed-off-by: Ivailo Monev <xakepa10@gmail.com>

--- okular/core/audioplayer.cpp
+++ okular/core/audioplayer.cpp
@@ -103,7 +103,9 @@ bool AudioPlayerPrivate::play( const SoundInfo& si )
     PlayData * data = new PlayData();
     data->m_player = new KAudioPlayer();
     data->m_player->setPlayerID("okulartpart_audio");
-    data->m_player->setVolume( float(si.volume) );
+    const float volume = float(si.volume * 100);
+    Q_ASSERT(volume >= 0.0 && volume <= 100.0);
+    data->m_player->setVolume( qBound<float>(0.0, volume, 100.0) );
     data->m_info = si;
     bool valid = false;
 
